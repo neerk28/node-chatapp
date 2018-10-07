437a475112e4e37c43a829512e0e7142fa258f00
neerk28 <neerk@cisco.com>
2018-10-07 12:45:29 +0530

    establish connection with client using socket.io

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.listen(port, () =>{
+var server = http.createServer(app);
+
+server.listen(port, () =>{
     console.log(`Server started in port ${port}`);
+});
+
+var io = socketIO(server); // creates a websocket server
+
+io.on('connection', (socket) => {
+    console.log('New connection');
+
+    socket.on('disconnect',() =>{
+        console.log('Disconnected from client');
+    })
 });
 
 app.use(express.static(path.join(__dirname,'../public')));
