5f5f9d780eac3c614988a6d24c2e653f8deff55f
neerk28 <neerk@cisco.com>
2018-10-14 13:12:40 +0530

    added styling and enabling and disabling send  location button

--- public/js/index.js
+++ public/js/index.js
@@ -12,7 +12,7 @@ socket.on('newMessage', (newMessage) => {
 socket.on('newLocationMessage', (location) => {
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My Current Location</a>');
-    li.text(`${location.from}:`)
+    li.text(`${location.from}: `)
     a.attr('href', location.url);
     li.append(a);
     jQuery('#messages').append(li);
@@ -22,31 +22,32 @@ socket.on('disconnect', () => {
     console.log('Disconnected from Server');
 })
 
+var messageBox = jQuery('[name=message]');
 jQuery('#input-message').on('submit', function(e){
     e.preventDefault(); // stop browser refreshing
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageBox.val()
     }, function(){
-
+        messageBox.val('');
     });
-})
+});
+
 var locationButton = jQuery('#send-location');
 locationButton.on('click', function(){
     if(!navigator.geolocation){
         return alert('Geolocation not supported in this browser');
     }
+    locationButton.attr('disabled','disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function(position){
-        console.log(position);
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             from: 'User',
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
-            
-        }, function(){
-    
         });
     },function(error){
+        locationButton.removeAttr('disabled').text('Send location');
         return alert('Unable to fetch location');
     })
     
